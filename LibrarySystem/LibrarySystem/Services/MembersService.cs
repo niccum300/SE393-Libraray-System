c99d063cdbd3814a1d6aaff8894cdda781a2b4f2
niccum300 <31547180+niccum300@users.noreply.github.com>
2021-02-17 22:25:38 -0500

    Added Lastname comparison for user search

--- LibrarySystem/LibrarySystem/Services/MembersService.cs
+++ LibrarySystem/LibrarySystem/Services/MembersService.cs
@@ -43,7 +43,7 @@ namespace LibrarySystem.Services
 
             using (ApplicationDbContext dbContext = _db.CreateDbContext())
             {
-                return await dbContext.LibraryMembers.Where(m => m.FirstName.ToLower().Contains(searchString.ToLower())).ToListAsync();
+                return await dbContext.LibraryMembers.Where(m => (m.FirstName.ToLower() + " " + m.LastName.ToLower()).Contains(searchString.ToLower())).ToListAsync();
             }
         }
 
